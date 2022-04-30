b6d386753e1f22d889d238a56ef22024d7f65976
aylok1n <nikolya.blog@gmail.com>
2022-04-30 13:22:13 +0400

    Add redirects

--- src/components/pages/changePassword.tsx
+++ src/components/pages/changePassword.tsx
@@ -1,4 +1,10 @@
+import { Navigate } from "react-router-dom"
+import { useAppSelector } from "../../redux/hooks"
+
 export const ChangePasswordPage = () => {
+    const user = useAppSelector((state) => state.user)
+    if (!user.email || !user.password) return <Navigate to={'/signIn'} />
+
     return (
         <main>ChangePassWordPage</main>
     )
--- src/components/pages/singnIn.tsx
+++ src/components/pages/singnIn.tsx
@@ -1,4 +1,10 @@
+import { Navigate } from "react-router-dom"
+import { useAppSelector } from "../../redux/hooks"
+
 export const SignInPage = () => {
+    const user = useAppSelector((state) => state.user)
+    if (!!user.email && !!user.password) return <Navigate to={'/'} />
+
     return (
         <main>SignInPage</main>
     )
