aba3bb8deb6e57cbc488c38eb421c0f39387a4fb
aylok1n <nikolya.blog@gmail.com>
2022-04-30 21:32:56 +0400

    Create main page

--- src/components/pages/welcome.tsx
+++ src/components/pages/welcome.tsx
@@ -1,7 +1,16 @@
 export const WelcomePage = () => {
   return (
-    <main>
+    <main className="text-center font-semibold">
       <p>welcome</p>
+      <p>
+        this is my test case for <strong>ciplay</strong>
+      </p>
+      <p>
+        my{" "}
+        <a className="underline hover:no-underline" target='_blank' href="https://github.com/aylok1n">
+          github
+        </a>
+      </p>
     </main>
   );
 };
