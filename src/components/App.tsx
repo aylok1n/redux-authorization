391195a8c329eead2d47d62ec4fda2315ab472bc
aylok1n <nikolya.blog@gmail.com>
2022-04-30 11:37:43 +0400

    Add tailwindcss

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,13 @@
 import { Link, Outlet } from 'react-router-dom';
-import '../styles/App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <nav>
-        <Link to="/">main</Link>
-        <Link to="/signIn">signIn</Link>
-        <Link to="/signUp">signUp</Link>
-        <Link to="/changePassword">changePassword</Link>
+    <div className='flex items-center flex-col'>
+      <nav className='w-full py-5 bg-blue-500 shadow-lg shadow-blue-500/50 align-middle font-bold text-white mb-3 '>
+        <Link className='self-start hover:text-cyan-100 float-left mx-2' to="/">Главная</Link>
+        <Link className='self-end hover:text-cyan-100 float-right mx-2' to="/signIn">Авторизацтя</Link>
+        <Link className='self-end hover:text-cyan-100 float-right mx-2' to="/signUp">Регистрация</Link>
+        <Link className='self-end hover:text-cyan-100 float-right mx-2' to="/changePassword">Сменить пароль</Link>
       </nav>
       <Outlet />
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import './index.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from './components/App';
 import { SignInPage } from './components/pages/singnIn';
